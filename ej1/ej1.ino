6dcc14154ce89b90eaf029c9e2124023d85caf83
GonzaloFernandez <fernandez.gfg@gmail.com>
2018-11-20 13:19:18 -0300

    fix cheto

--- ej1/ej1.ino
+++ ej1/ej1.ino
@@ -3,21 +3,18 @@
 const int ledPIN_G = 8;
 const int ledPIN_Y = 7;
 const int ledPIN_R = 6;
- int p1 = 2;
+int p1 = 2;
 const int p2 = 3;
 const int dx=0.2;   //distancia entre pulsadores
 int ledState = LOW;
-int ledState1 = LOW; // El LED empieza apagado
-int ledState2 = LOW;
-volatile unsigned long blinkCount = 0; // Somos muy obedientes
-unsigned long previousMillis=0; // millis() returns an unsigned long.
- float vmax=16.7;  //velocidad max preestablecida de 60 km/h
-  long pausa=10000000;
-    bool flag1=true;
-  int high1 = LOW;
-  unsigned long tiempop1;
-  unsigned long tiempop2;
-  unsigned long interval = 2000000;
+float vmax=16.7;  //velocidad max preestablecida de 60 km/h
+uint16_t pausa = 10000000;
+bool flag = false;
+uint16_t tiempop1;
+uint16_t tiempop2;
+unsigned long tiempo_verif;
+unsigned long interval = 2000000;
+unsigned long MAX_TIME = 5000000;
   
 void setup() {
   Serial.begin(9600);    //iniciar puerto serie
@@ -34,7 +31,11 @@ void setup() {
 }
  
 void loop(){
-  
+  tiempo_verif = millis();
+  if (flag && (tiempo_verif - tiempop1) > MAX_TIME){
+    flag = 0;
+    Serial.print(":IGN\n\r");
+  }
 }
 
 void ISR_Blink(){
@@ -42,24 +43,36 @@ void ISR_Blink(){
   digitalWrite(ledPIN_Y, ledState);
 }
 void ISR_P1(){
+  flag = true;
   tiempop1 = millis();
   Serial.print(tiempop1);
   Serial.print("\n\r");
+  delay(100); // Antirrebote
 }
 
 void ISR_P2(){
-    tiempop2 = millis();
-    Serial.print(tiempop2);
-    Serial.print("\n\r");
-    if ((unsigned long)(tiempop1-tiempop2) < interval){
-        digitalWrite(ledPIN_Y, LOW);
-        digitalWrite(ledPIN_R, HIGH);
-        delay(10000000);
-        digitalWrite(ledPIN_R, LOW);
-    } else if ((unsigned long)(tiempop1 - tiempop2) > interval){
-        digitalWrite(ledPIN_Y, LOW);
-        digitalWrite(ledPIN_G, HIGH);
-        delay(10000000);
-        digitalWrite(ledPIN_G, LOW);
+    if (flag){
+      tiempop2 = millis();
+      Serial.print(tiempop2);
+      Serial.print("\n\r");
+      if ((tiempop2-tiempop1) < interval){
+          Serial.print(":CD1\n\r");
+          digitalWrite(ledPIN_Y, LOW);
+          digitalWrite(ledPIN_R, HIGH);
+          delay(pausa);
+          digitalWrite(ledPIN_R, LOW);
+      } else if ((tiempop2 - tiempop1) > interval){
+          Serial.print(":CD2\n\r");
+          digitalWrite(ledPIN_Y, LOW);
+          digitalWrite(ledPIN_G, HIGH);
+          delay(pausa);
+          digitalWrite(ledPIN_G, LOW);
+      }
+      flag = false;
+    } else {
+      Serial.print(":V");
+      Serial.print(vmax);
+      Serial.print("\n\r;");
     }
+    delay(100);   //antirrebote
 }
